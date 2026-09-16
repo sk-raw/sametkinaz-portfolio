327b760ec285182ee45a4bc30cad2677db548d23
Samet Kınaz <sametkinazphoto@gmail.com>
2026-09-16 16:17:30 +0300

    U14 zafer idman albümünün kapak fotoğrafı güncellendi

--- app/albumler/page.tsx
+++ app/albumler/page.tsx
@@ -73,7 +73,7 @@ export default function AlbumlerSayfasi() {
       id: 6,
       title: "U14 Zeytinburnu Zaferspor - İdman",
       date: "04 Eylül 2026",
-      coverImage: "/u14zaferidman/u14zaferidman1.jpg",
+      coverImage: "/u14zaferidman/u14zaferidman2jpg",
       colSpan: "col-span-1",
       photos: [
         "/u14zaferidman/u14zaferidman2.jpg", "/u14zaferidman/u14zaferidman3.jpg",
