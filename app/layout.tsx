24f50e4d119edec4d07455f6ae9249c54fabbb6a
Samet Kınaz <sametkinazphoto@gmail.com>
2026-09-16 11:54:30 +0300

    google analytics eklendi

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <Analytics />
+      <GoogleAnalytics gaId="G-JNMS85F20H" />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
